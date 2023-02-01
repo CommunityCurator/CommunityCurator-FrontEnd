eda3080d4688d3e909d0ef8cbca82dd4da3edba0
Mariia Ellis <mvoro001@odu.edu>
2023-01-31 22:31:18 -0500

    Mariia updated sign in form

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -13,16 +13,46 @@ export default function Login() {
   return(
     <>
     <div align="center">
-      <form>
-        <label for="email">email</label>
-        <input defaultValue={email} type="email" placeholder='youremail@email.com' id="email" name="email"/>
-
-        <label for="password">passwordl</label>
-        <input  defaultValue={password} type="password" placeholder='***********' id="password" name="password"/>
-        <button type="submit">Login</button>
-      </form>
-      <button>Don't have an account? Register here.</button>
+    <form className="w-full max-w-sm" >
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
+          <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+            Email
+          </label>
+        </div>
+        <div className="md:w-2/3">
+          <input defaultValue={email} className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="inline-email" type="text" placeholder="email"/>
+        </div>
       </div>
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
+          <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
+            Password
+          </label>
+        </div>
+        <div className="md:w-2/3">
+          <input defaultValue={password} className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="inline-password" type="password" placeholder="******************"/>
+        </div>
+      </div>
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3"></div>
+          <label className="md:w-2/3 block text-gray-500 font-bold">
+            <input className="mr-2 leading-tight" type="checkbox"/>
+              <span className="text-sm">
+                Stay signed in
+              </span>
+          </label>
+        </div>
+      <div className="md:flex md:items-center">
+      <div className="md:w-1/3"></div>
+        <div className="md:w-2/3">
+          <button className="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+            Log in
+          </button>
+        </div>
+      </div>
+  </form>
+  </div>
     </>
   );
 
