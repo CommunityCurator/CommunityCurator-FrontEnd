07a37a2499277814dbd4bbc360f590a0e99f8e23
Mariia Ellis <mvoro001@odu.edu>
2023-02-01 13:35:15 -0500

    Mariia added register button on signup page

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -13,54 +13,54 @@ export default function Login() {
   return(
     <>
     <div align="center">
-    <form className="w-full max-w-sm" >
-      <div className="md:flex md:items-center mb-6">
-        <div className="md:w-1/3">
-          <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="email">
-            Email
-          </label>
+      <form className="m-2 py-8 px-8 w-full max-w-sm" >
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="email">
+              Email
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input defaultValue={email} className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="inline-email" type="text" placeholder="email"/>
+          </div>
         </div>
-        <div className="md:w-2/3">
-          <input defaultValue={email} className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="inline-email" type="text" placeholder="email"/>
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="password">
+              Password
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input defaultValue={password} className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="inline-password" type="password" placeholder="******************"/>
+          </div>
         </div>
-      </div>
-      <div className="md:flex md:items-center mb-6">
-        <div className="md:w-1/3">
-          <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="password">
-            Password
-          </label>
-        </div>
-        <div className="md:w-2/3">
-          <input defaultValue={password} className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="inline-password" type="password" placeholder="******************"/>
-        </div>
-      </div>
-      <div className="md:flex md:items-center mb-6">
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3"></div>
+            <label className="md:w-2/3 block text-gray-500 font-bold">
+              <input className="mr-2 leading-tight" type="checkbox"/>
+                <span className="text-sm">
+                  Stay signed in
+                </span>
+            </label>
+          </div>
+        <div className="md:flex md:items-center">
         <div className="md:w-1/3"></div>
-          <label className="md:w-2/3 block text-gray-500 font-bold">
-            <input className="mr-2 leading-tight" type="checkbox"/>
-              <span className="text-sm">
-                Stay signed in
-              </span>
-          </label>
+          <div className="md:w-2/3">
+            <button className="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+              Log in
+            </button>
+          </div>
         </div>
-      <div className="md:flex md:items-center">
-      <div className="md:w-1/3"></div>
-        <div className="md:w-2/3">
-          <button className="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
-            Log in
-          </button>
-        </div>
-      </div>
 
-      <div className=" block mx-auto md:flex md:items-center mb-6">
-        <div className="md:w-1/3"></div>
+        <div className=" block mx-auto md:flex md:items-center mb-6">
+          <div className="md:w-1/3"></div>
           <label className="md:w-2/3 block text-gray-500 font-bold">
               <p className="text-sm">
-                Don't have an account? Register <a href='/signup'><u>here</u></a>
+                Don't have an account? <a href='/signup'><u> Register</u></a>
               </p>
           </label>
         </div>
-  </form>
+    </form>
   </div>
     </>
   );
--- src/components/pages/Signup.js
+++ src/components/pages/Signup.js
@@ -13,8 +13,8 @@ export default function Signup() {
     return(
     <>
     <div align="center">
-        <form className="w-full max-w-lg">
-            <div className="flex flex-wrap -mx-3 mb-6">
+        <form className="m-2 py-8 px-8 w-full max-w-lg">
+            <div className="flex flex-wrap -mx-3 mb-1">
                 <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                     <label className="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4" for="firstname">
                         First Name
@@ -28,7 +28,7 @@ export default function Signup() {
                     <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Last Name" />
                 </div>
             </div>
-            <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="flex flex-wrap -mx-3 mb-1">
                 <div className="w-full px-3">
                     <label className="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4" for="password">
                         Password
@@ -57,8 +57,27 @@ export default function Signup() {
                     <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="Zip code"/>
                 </div>
             </div>
+
+            <div className="py-3 px-3 md:flex md:items-center">
+                <div className="md:w-1/3"></div>
+                <div className="md:w-1/3">
+                    <button className="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+                        Register
+                    </button>
+                </div>
+            </div>
+            <div className=" block mx-auto md:flex md:items-center mb-6">
+                <div className="md:w-1/3 "></div>
+                <label className="md:w-2/3 block text-gray-500 font-bold">
+                    <p className="text-sm">
+                        Already have an account? <a href='/login'><u>Log in</u></a>
+                    </p>
+                </label>
+             </div>
+             
         </form>
     </div>
+
 </>
 );
 }
