3a695c22a149e4a25abf9078f10e3b04b14253d2
Joseph Arias <jaria001@odu.edu>
2023-04-05 10:21:16 -0400

    created EventCard component

---
+++ src/components/EventCard/EventCard.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Typography from '@mui/material/Typography';
+
+
+const EventCard = (props) => {
+  debugger
+  return (
+    <Card sx={{ maxWidth: 250, height: 350 }}>
+      <CardMedia
+        component="img"
+        style={{height: '140px'}}
+        image={props.event.images[0].url}
+      />
+       <CardHeader
+        title={props.event.name}
+        // subheader={city}
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+         {/* {description} */}
+        </Typography>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default EventCard;
