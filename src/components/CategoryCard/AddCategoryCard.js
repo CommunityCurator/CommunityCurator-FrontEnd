cc507212f514779dcf54fed1336f99d5e8ba97b1
Mariia Ellis <mvoro001@odu.edu>
2023-04-13 10:43:32 -0400

    after choosing category card is hidden

--- src/components/CategoryCard/AddCategoryCard.js
+++ src/components/CategoryCard/AddCategoryCard.js
@@ -7,69 +7,108 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { set } from 'react-hook-form';
 
-
 const AddCategoryCard = (props) => {
-  const [isHover, setIsHover] = useState(false)
+  const [isHover, setIsHover] = useState(false);
+  const [isAdded, setIsAdded] = useState(false);
 
-  function addInterest() {
-    const userId = parseInt(localStorage.getItem('currentUser'))
+  useEffect(() => {
+    const userId = parseInt(localStorage.getItem('currentUser'));
     const url1 = 'http://localhost:8000/api/categories';
     let url2 = `http://127.0.0.1:8000/api/user/${userId}/categories`;
 
     fetch(url1)
-    .then((response) => {
-        return response.json()
-    })
-    .then(data => {
-        for(var key in data.categories) {
-            if(data.categories[key].name == props.name) {
-                const id = props.id
-                url2 += "/" + data.categories[key].id
-                console.log("**********")
-                console.log(url2)
-                return url2
-            }
-        }
-    })
-    .then(url => {
-      fetch(url, {
-        method: "POST",
-        headers : {      
-            'Content-Type': 'application/json'
-        }
-      })
       .then((response) => {
-          console.log(url)
-          console.log(props.id)
+        return response.json();
       })
-      
-    })
+      .then((data) => {
+        for (var key in data.categories) {
+          if (data.categories[key].name === props.name) {
+            const id = props.id;
+            url2 += '/' + data.categories[key].id;
+            fetch(url2, {
+              method: 'GET',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            })
+              .then((response) => {
+                return response.json();
+              })
+              .then((data) => {
+                if (data.length > 0) {
+                  setIsAdded(true);
+                }
+              });
+          }
+        }
+      });
+  }, []);
+
+  function addInterest() {
+    const userId = parseInt(localStorage.getItem('currentUser'));
+    const url1 = 'http://localhost:8000/api/categories';
+    let url2 = `http://127.0.0.1:8000/api/user/${userId}/categories`;
+
+    fetch(url1)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        for (var key in data.categories) {
+          if (data.categories[key].name === props.name) {
+            const id = props.id;
+            url2 += '/' + data.categories[key].id;
+            fetch(url2, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            })
+              .then((response) => {
+                setIsAdded(true);
+              });
+          }
+        }
+      });
+  }
+
+  if (isAdded) {
+    return null;
   }
 
   return (
     <>
-      <Card variant="outlined" style={{margin: '1em'}}>
-      <CardContent>
-        {isHover == false && (        
-        <Typography style={{textAlign: 'center'}} sx={{ fontSize: 18 }} color="text.secondary" gutterBottom
-                onMouseEnter={() => setIsHover(true)}
-                onMouseLeave={() => setIsHover(false)}>
-            {props.name}
-        </Typography>)}
+      <Card variant="outlined" style={{ margin: '1em' }}>
+        <CardContent>
+          {isHover == false && (
+            <Typography
+              style={{ textAlign: 'center' }}
+              sx={{ fontSize: 18 }}
+              color="text.secondary"
+              gutterBottom
+              onMouseEnter={() => setIsHover(true)}
+              onMouseLeave={() => setIsHover(false)}
+            >
+              {props.name}
+            </Typography>
+          )}
 
-        {isHover && (
-        <Typography style={{textAlign: 'center'}} sx={{ fontSize: 18 }} color="text.secondary" gutterBottom
-            onMouseEnter={() => setIsHover(true)}
-            onMouseLeave={() => setIsHover(false)}>
-            <button onClick={addInterest}>
-                Add Interest
-            </button>
-        </Typography>)
-        }
-      </CardContent>
+          {isHover && (
+            <Typography
+              style={{ textAlign: 'center' }}
+              sx={{ fontSize: 18 }}
+              color="text.secondary"
+              gutterBottom
+              onMouseEnter={() => setIsHover(true)}
+              onMouseLeave={() => setIsHover(false)}
+            >
+              <button onClick={addInterest}>Add Interest</button>
+            </Typography>
+          )}
+        </CardContent>
       </Card>
     </>
-  )
-}
+  );
+};
 
 export default AddCategoryCard;
